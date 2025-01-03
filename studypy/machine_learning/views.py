d52eddae2e74d7313eddb170cd3df522f5de3f16
nothing36 <prodipsarker953@gmail.com>
2025-01-04 00:50:52 +0600

    first commit

---
+++ studypy/machine_learning/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def machine_learning(request):
+    return HttpResponse('<h2>This a simple django project.</h2>')
